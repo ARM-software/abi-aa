c7c1bf8f9d7ca61569c7848f6344aed6cf21e520
Ties Stuij <ties.stuij@arm.com>
2020-09-11 03:06:10 +0100

    add exception handling abi document
    - also adding multi-line footer
    - cleaned up the document format a bit, adding more variables at front of
      document

--- tools/rst2pdf/generate-pdfs.sh
+++ tools/rst2pdf/generate-pdfs.sh
@@ -11,8 +11,15 @@ ABI_ROOT=${CURR_DIR}/../..
 OUTPUT_DIR=$1
 
 mkdir -p ${OUTPUT_DIR}
+
+# 32-bit
+rst2pdf ${ABI_ROOT}/ehabi32/ehabi32.rst -s ${CURR_DIR}/rst2pdf-abi.style \
+        --repeat-table-rows --footer="###Page###" -o ${OUTPUT_DIR}/ehabi32.pdf
+
+# 64-bit
 rst2pdf ${ABI_ROOT}/aapcs64/aapcs64.rst -s ${CURR_DIR}/rst2pdf-abi.style \
         --repeat-table-rows --default-dpi=110 --footer="###Page###" -o ${OUTPUT_DIR}/aapcs64.pdf
+
 rst2pdf ${ABI_ROOT}/vfabia64/vfabia64.rst -s ${CURR_DIR}/rst2pdf-abi.style \
         --repeat-table-rows --footer="###Page###" -o ${OUTPUT_DIR}/vfabia64.pdf
 rst2pdf ${ABI_ROOT}/pauthabielf64/pauthabielf64.rst -s ${CURR_DIR}/rst2pdf-abi.style \
