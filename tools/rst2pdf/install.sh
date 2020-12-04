4b5dd8aa007b4410dd1c424194e3ab0cf9536a41
Ties Stuij <ties.stuij@arm.com>
2020-12-04 14:48:46 +0000

    github actions support

--- tools/rst2pdf/install.sh
+++ tools/rst2pdf/install.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
+cd $(dirname $BASH_SOURCE)
 
 # we need a very recent version of rst2pdf, as (currently) the latest release
 # installed with Ubuntu 16.04 contains a bug where the contents section links
 # are broken
 # the rst2pdf release below is 0.98
-pip install git+git://github.com/rst2pdf/rst2pdf@d9bf8cd737c11cfc572c936173228c1103344fdf
-pip install rstcheck matplotlib svglib
+pip install -r requirements.txt
