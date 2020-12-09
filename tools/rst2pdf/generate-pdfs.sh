45a122c0b48a242144d727c9837e045cfc2e8fa0
Amilendra Kodithuwakku <amilendra.kodithuwakku@arm.com>
2020-12-09 17:12:08 +0000

    [aapcs64-morello] add Morello extensions to PCS for AArch64 document

--- tools/rst2pdf/generate-pdfs.sh
+++ tools/rst2pdf/generate-pdfs.sh
@@ -23,7 +23,7 @@ declare -a docs=(
     "pauthabielf64"
 
     # morello
-    "aadwarf64-morello" "aaelf64-morello"
+    "aadwarf64-morello" "aaelf64-morello" "aapcs64-morello"
 )
 
 for doc in "${docs[@]}"; do
